7182fddda0750599edef7105a357281150c9f2af
sjjk <stefan.konzett@gmail.com>
2016-02-29 12:24:01 -0500

    more flexible input argument processing - can run now from bin/Release directory

--- src/H/inputArguments.h
+++ src/H/inputArguments.h
@@ -8,8 +8,17 @@ string pathToCase="./case/"+Pa.caseName;
 verbosity(Pa,"check if we have a true case directory at "+pathToCase,2,__FILE__,__LINE__);
 string paramFile=pathToCase+"/inputFile.param";
 ifstream f(paramFile.c_str());
-myFunctions::cassert(f.good(),ISCRITICAL,"could not find a inputFile.param file in directory "+ pathToCase +" - Exit!",__FILE__,__LINE__);
+if(!f.good())
+{
 f.close();
+verbosity(Pa,"maybe we are in bin/Release folder - check this "+pathToCase,2,__FILE__,__LINE__);
+string pathToCase="../../case/"+Pa.caseName;
+verbosity(Pa,"check if we have a true case directory at "+pathToCase,2,__FILE__,__LINE__);
+string paramFile=pathToCase+"/inputFile.param";
+ifstream f2(paramFile.c_str());
+myFunctions::cassert(f2.good(),ISCRITICAL,"could not find a inputFile.param file in input directory and in directory "+ pathToCase +" - Exit!",__FILE__,__LINE__);
+f2.close();
+}
 verbosity(Pa,"our input file for this run is "+paramFile,2,__FILE__,__LINE__);
 
 #endif // INPUTARGUMENTS_H_INCLUDED
