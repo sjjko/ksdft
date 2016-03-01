0560a533ca4e5781fb5c34ebf3a6cdc60c327e37
sjjk <stefan.konzett@gmail.com>
2016-02-29 22:49:00 -0500

    doxygen update

--- doc/html/search/all_12.js
+++ doc/html/search/all_12.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['_7eci',['~cI',['../classc_i.html#a7fb4f04615a33e12d8497a9f07026371',1,'cI']]],
+  ['_7ecjdag',['~cJdag',['../classc_jdag.html#aa228bb1389a650a6404f0a1d1b6e973e',1,'cJdag']]],
   ['_7elop',['~Lop',['../class_lop.html#a6f917e9236f7f9a98b343e02bcf64715',1,'Lop']]]
 ];
